235f1558657098051bdb07e7a92e6481029a4453
Ivan <petrovic.valpovo@gmail.com>
2023-09-27 14:35:50 +0100

    Add function to flip the tile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // Call the function to run the game when the page loads
 document.addEventListener("DOMContentLoaded", runGame);
 let username = document.getElementById("username").innerText = window.prompt("Enter Your Username!");
+
 function runGame() {
 
     // Select all elements with the class "tile" and other necessary elements
@@ -25,6 +26,35 @@ function runGame() {
             tile.style.order = randomPos;
         });
     }
+
+    // Function to flip a tile 
+    function flipTile(tile) {
+        // Check if the tile is not already flipped and there are fewer than 2 flipped tiles
+        if (!tile.classList.contains("flipped") && flippedTiles.length < 2) {
+            // Add the flipped class to the tile
+            tile.classList.add("fliped");
+            // Find the inner tile element
+            const tileInner = document.querySelector(".tile-inner");
+            // Find the image within the tile
+            const image = tile.querySelector("img");
+            // Show the tile background and add the class "visible"
+            tileInner.classList.add("visible");
+            // Show the image and remove the class "hidden"
+            image.classList.remove("hidden");
+            // Add a CSS transform style to rotate the tile
+            tile.style.transform = "rotateY(-180deg)";
+            // Add the flipped tile to the array
+            flippedTiles.push(tile);
+        }
+
+    }
+
+    // Event listener for each tile to flip and reveal the image
+    tiles.forEach(tile => {
+        tile.addEventListener("click", () => {
+            flipTile(tile);
+        })
+    })
 
     // Function called to shuffle the tiles
     shuffleTiles();
